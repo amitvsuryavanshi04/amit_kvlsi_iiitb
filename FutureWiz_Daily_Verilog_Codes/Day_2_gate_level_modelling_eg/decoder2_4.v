522e38a663ab03b90ec4418c38601038c3b7ca56
AMIT SURYAVANSHI <108608256+amitvsuryavanshi04@users.noreply.github.com>
2025-04-09 17:53:49 +0530

    Update decoder2_4.v

--- FutureWiz_Daily_Verilog_Codes/Day_2_gate_level_modelling_eg/decoder2_4.v
+++ FutureWiz_Daily_Verilog_Codes/Day_2_gate_level_modelling_eg/decoder2_4.v
@@ -1,1 +1,13 @@
-
+module decoder2_4(i,d);
+input [1:0]i;
+output [3:0]d;
+//intermediate signals
+wire w1,w0;
+//output signals
+not n1(w1,i[1]);
+not n2(w0,i[0]);
+and a1(d[0],w1,w0);
+and a2(d[1],w1,i[0]);
+and a3(d[2],w2,i[1]);
+and a4(d[3],i[1],i[0]);
+endmodule
