08357b7a71c034d6dadf5be0d04ddf7a15fd623c
AMIT SURYAVANSHI <108608256+amitvsuryavanshi04@users.noreply.github.com>
2025-04-12 12:17:59 +0530

    Update mux_41_dataflow.v

--- FutureWiz_Daily_Verilog_Codes/Day_03_data_flow_and_tb/mux41/mux_41_dataflow.v
+++ FutureWiz_Daily_Verilog_Codes/Day_03_data_flow_and_tb/mux41/mux_41_dataflow.v
@@ -1,8 +1,9 @@
+// Code your design here
 module mux41_gate(i,s,y);
   input [3:0]i;
   input [1:0]s;
   output y;
 
   //data flow execution
-  assign y = (i[0]&(~s[1])&(~s[1]))|(i[1]&(~s[1])&s[0])|(i[1]&(~s[0])&s[0])|(i[1]&(s[1])&s[0]);
+  assign y = (i[0]&(~s[1])&(~s[0]))|(i[1]&(~s[1])&s[0])|(i[2]&(s[1])&(~s[0]))|(i[3]&(s[1])&s[0]);
 endmodule
