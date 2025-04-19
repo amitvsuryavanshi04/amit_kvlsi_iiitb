3290dfa5f33f80fb9efe788935bf20f3d6cd73c3
AMIT SURYAVANSHI <108608256+amitvsuryavanshi04@users.noreply.github.com>
2025-04-19 18:28:01 +0530

    added day-8 files

---
+++ FutureWiz_Daily_Verilog_Codes/Day_08_practice_question/d_latch_using_mux21/dl_using_mux.v
@@ -0,0 +1,20 @@
+
+module mux21(i[0],i[1],s,y);
+input [1:0]i;
+input s;
+output y;
+assign y = (s) ?i[1]:i[0];
+endmodule
+
+module dlatch(d,clk,q);
+input clk,d;
+output q;
+
+wire q_int1;
+// wire q_int2;
+
+mux21 m1(q_int1,d,clk,q_int1);
+//mux21 m2(q_int2,q_int1,~clk,q_int2); // uncomment the commented lines to get the code for d-flipflop
+
+assign q = q_int1;
+endmodule
