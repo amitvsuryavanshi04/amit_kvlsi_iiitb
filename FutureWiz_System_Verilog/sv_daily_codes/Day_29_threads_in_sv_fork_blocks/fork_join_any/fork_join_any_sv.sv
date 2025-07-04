3a0b398ae20f20cab713e76e0439a0ddf4a5f7d2
AMIT SURYAVANSHI <108608256+amitvsuryavanshi04@users.noreply.github.com>
2025-07-04 17:19:21 +0530

    rem1

---
+++ FutureWiz_System_Verilog/sv_daily_codes/Day_29_threads_in_sv_fork_blocks/fork_join_any/fork_join_any_sv.sv
@@ -0,0 +1,21 @@
+//-------------------------------------------------------------------//
+//fork......join_any example 
+
+module test_fork_join_any;
+  initial begin 
+    #1 $display("[%0t], the fork join block begins.", $time);
+    
+    fork 
+      #5 $display("[%0t], The thread1 is executed.", $time);
+      
+      begin // the two statements belong to thread2
+        #2 $display("[%0t], The thread2 1st statement is executed.", $time);
+        #4 $display("[%0t], The thread2 2nd statement is executed.", $time);
+      end
+      //th3
+      #10 $display("[%0t], The thread3 is executed.", $time);
+    join_any //end of fork join block
+    
+    $display("[%0t], The fork join block is exited.", $time);
+  end 
+endmodule 
---
+++ FutureWiz_System_Verilog/sv_daily_codes/Day_29_threads_in_sv_fork_blocks/fork_join_none/fork_join_none_sv.sv
@@ -0,0 +1,20 @@
+
+//-------------fork...join_none example 
+module test_fork_join_none;
+  initial begin 
+    #1 $display("[%0t], the fork join block begins.", $time);
+    
+    fork 
+      #5 $display("[%0t], The thread1 is executed.", $time);
+      
+      begin // the two statements belong to thread2
+        #2 $display("[%0t], The thread2 1st statement is executed.", $time);
+        #4 $display("[%0t], The thread2 2nd statement is executed.", $time);
+      end
+      //th3
+      #10 $display("[%0t], The thread3 is executed.", $time);
+    join_none //end of fork join block
+    
+    $display("[%0t], The fork join block is exited.", $time);
+  end 
+endmodule 
